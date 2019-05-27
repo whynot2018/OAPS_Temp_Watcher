4696c9643af240913f0771580ca5b2efec7b512a
whynot2018 <48062504+whynot2018@users.noreply.github.com>
2019-05-27 22:18:03 +0200

    Update themp.sh

--- themp.sh
+++ themp.sh
@@ -10,7 +10,7 @@ gpu=$(vcgencmd measure_temp)
 arm=$(vcgencmd measure_clock arm)
 volt=$(vcgencmd measure_volts core)
 echo -e "${bold}${green}$(date) @ $(whoami) @ $UID @ $(hostname)"
-echo -e "${bold}${green}$arm @ $volt"
+echo -e "${bold}${green}Core $arm @ $volt"
 echo -e "${bold}${green}-------------------------------------------${nc}"
 echo -e "${bold}${blue}GPU${normal} => ${red}$gpu${nc}"
 echo -e "${bold}${blue}CPU${normal} => ${red}$((cpu/1000))'C${nc}"
